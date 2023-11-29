20a8a4b747b325f431eef8f331340565d875a279
devon-caron <devon.caron.p@gmail.com>
2023-11-29 12:57:07 -0800

    Added controllers

---
+++ src/main/java/com/cooksys/socialmedia/controllers/HashtagController.java
@@ -0,0 +1,12 @@
+package com.cooksys.socialmedia.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+//@RequestMapping()
+public class HashtagController {
+
+}

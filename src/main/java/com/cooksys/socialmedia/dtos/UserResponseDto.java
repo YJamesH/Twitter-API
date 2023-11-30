9cfe652adf6ee63189f6f870d5625f595a4537df
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-29 19:16:51 -0600

    Update UserResponseDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/UserResponseDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/UserResponseDto.java
@@ -3,9 +3,13 @@ package com.cooksys.socialmedia.dtos;
 import java.security.Timestamp;
 
 import com.cooksys.socialmedia.entities.Profile;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 public class UserResponseDto {
 	private String username; //req
-	private Profile profile; //req
+	private ProfileDto profile; //req
 	private Timestamp joined; //req
 }

65685fc273cf96602da9d0e2795d242794d128b2
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-29 19:15:23 -0600

    Update TweetRequestDto.java

--- src/main/java/com/cooksys/socialmedia/dtos/TweetRequestDto.java
+++ src/main/java/com/cooksys/socialmedia/dtos/TweetRequestDto.java
@@ -1,5 +1,9 @@
 package com.cooksys.socialmedia.dtos;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 public class TweetRequestDto {
 	private String content;
 	private CredentialsDto credentials;

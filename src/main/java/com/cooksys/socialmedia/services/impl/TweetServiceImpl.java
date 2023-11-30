d8669a18c67f55adfda01c4ebc383170b0013d89
Helena Makendengue <HelenaMakens1013@gmail.com>
2023-11-29 20:11:44 -0600

    Update TweetServiceImpl.java

--- src/main/java/com/cooksys/socialmedia/services/impl/TweetServiceImpl.java
+++ src/main/java/com/cooksys/socialmedia/services/impl/TweetServiceImpl.java
@@ -4,9 +4,10 @@ import org.springframework.stereotype.Service;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@AllArgsConstructor
+import lombok.RequiredArgsConstructor;
+
 @Service
-@Data
-public class TweetServiceImpl {
+@RequiredArgsConstructor
+public class TweetServiceImpl implement TweetService {
 
 }
